38d515e78dc56c22075c715d838ba6ade4a18791
Andrew Moshi <andrewemoshi@gmail.com>
2022-08-02 16:45:17 +0300

    second push

--- btre/settings.py
+++ btre/settings.py
@@ -155,12 +155,6 @@ MESSAGE_TAGS = {
 # Email config
 
 # Email Sendgrid Config
-# SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
 
 
